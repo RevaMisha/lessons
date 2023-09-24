433d3368be2cf7b25ef0c9891f7d80202fbc0222
Mykhailo Reva <112630176+RevaMisha@users.noreply.github.com>
2023-09-24 18:27:26 +0300

    Add files via upload

---
+++ lesson_20!/js/script.js
@@ -0,0 +1,120 @@
+
+document.addEventListener('click', function (e) {
+	const targetElement = e.target;
+
+	if (targetElement.closest('.menu-burger')) {
+		document.documentElement.classList.toggle('menu-open');
+		e.preventDefault();
+	}
+
+})
+
+document.querySelector('.menu-burger').addEventListener('click', function() {
+	this.classList.toggle('active');
+	
+});
+
+
+
+//  Слайдер свайпер
+
+new Swiper(".swiper-container", {
+	navigation: {
+		nextEl: '.swiper-button-next', 
+		prevEl: '.swiper-button-prev'
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		
+	  },
+
+	  loop: true,
+	  speed : 1500,
+	  effect: 'cube',
+    
+	
+});
+
+
+
+const gal = new Swiper(".gal", {
+	  autoplay: {
+	  loop: true,
+	  delay: 2000,
+	},
+	breakpoints: {
+	  320: {
+		slidesPerView: 1, 
+		spaceBetween: 10,
+	  },
+	  450: {
+		slidesPerView: 2, 
+		spaceBetween: 15,
+	  },
+
+	  680: {
+		slidesPerView: 3, 
+		spaceBetween: 15,
+	  },
+	  
+	},
+  });
+  
+
+// Підключення реєстрації 
+
+const wrapper = document.querySelector('.wrap');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const headerRegistratio = document.querySelector('.header__registratio');
+const iconClose = document.querySelector('.acon-close');
+
+  
+registerLink.addEventListener('click', ()=> {
+	wrapper.classList.add('active');
+}); 
+  
+loginLink.addEventListener('click', ()=> {
+	wrapper.classList.remove('active');
+}); 
+
+headerRegistratio.addEventListener('click', () => {
+	wrapper.classList.add('active-registratio');
+	document.documentElement.classList.add('form-open');
+  });
+  
+  iconClose.addEventListener('click', () => {
+	wrapper.classList.remove('active-registratio');
+	document.documentElement.classList.remove('form-open');
+  });
+
+
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+	
+	
+  
+  
